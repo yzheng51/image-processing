f8251879610454ffffbb7a158e4192e2adba4d42
yzheng51 <y.zheng211@gmail.com>
2021-02-25 00:08:48 +0800

    Optimization CPU OPENMP speed

--- src/filter.c
+++ src/filter.c
@@ -9,40 +9,11 @@
  * @copyright Copyright (c) 2019, yzheng
  *
  */
+#include <time.h>
 #include <omp.h>
 #include <stdlib.h>
 
 #include "filter.h"
-
-/**
- * @Allocate memory and initialise the rgb value for lpixel matrices
- *
- * @param cols
- * @param rows
- * @return lpixel**
- */
-lpixel *init_lpixel(int cols, int rows) {
-    lpixel *pixels = NULL;
-
-    pixels = (lpixel *)calloc(rows * cols, sizeof(lpixel));
-    // check whether the allocation is successful, the same in loop
-    if (pixels == NULL) {
-        fprintf(stderr, "Error: Memory allocation failed.\n");
-        exit(1);
-    }
-
-    return pixels;
-}
-
-/**
- * @Free lpixel matrices
- *
- * @param pixels
- * @param rows
- */
-void free_lpixel(lpixel *pixels) {
-    free(pixels);
-}
 
 /**
  * @Mosaic filter
@@ -58,165 +29,45 @@ void free_lpixel(lpixel *pixels) {
  * @param c
  */
 void mosaic_transform(pixel *pixels_o, pixel *pixels_i, int cols, int rows, int c) {
-    int i, j;                                           // loop counter
-    int mod_cols = cols % c;                            // remain part of width
-    int mod_rows = rows % c;                            // remain part of height
-    int m_cols = (mod_cols) ? cols / c + 1 : cols / c;  // check whether cols (rows) can be divided by c
-    int m_rows = (mod_rows) ? rows / c + 1 : rows / c;  // if not, plus one to ensure enough memory has benn allocated
-    lpixval m_area = c * c;
-    lpixval area = cols * rows;
-    lpixval average_r, average_g, average_b;
-    lpixel *lpixels;  // pixel for calculation
-    double begin, duration;
+    int x, y, i, j;                                     // loop counter
+    int m_rows = (rows % c) ? rows / c + 1 : rows / c;  // check whether cols (rows) can be divided by c
+    int m_cols = (cols % c) ? cols / c + 1 : cols / c;  // if not, plus one to ensure enough memory has benn allocated
+    int area = cols * rows;
+    unsigned long long average_r, average_g, average_b;
+    unsigned long long block_sum_r, block_sum_g, block_sum_b;
+    clock_t begin, end;
 
     average_r = 0; average_g = 0; average_b = 0;
 
-    // memory allocation and value initialisation
-    lpixels = init_lpixel(m_cols, m_rows);
-
-    begin = omp_get_wtime();
+    begin = clock();
     // sum up all rgb values and stor in the average pixel
-    for (i = 0; i < rows; ++i) {
-        for (j = 0; j < cols; ++j) {
-            int offset = i * cols + j;
-            int avg_offset = i / c * m_cols + j / c;
-            lpixels[avg_offset].r += pixels_i[offset].r;
-            lpixels[avg_offset].g += pixels_i[offset].g;
-            lpixels[avg_offset].b += pixels_i[offset].b;
-        }
-    }
+    for (x = 0; x < m_rows; ++x) {
+        for (y = 0; y < m_cols; ++y) {
+            int x_rows = (x == m_rows - 1) ? rows - x * c : c;
+            int y_cols = (y == m_cols - 1) ? cols - y * c : c;
+            int m_area = x_rows * y_cols;
+            block_sum_r = 0, block_sum_g = 0, block_sum_b = 0;
 
-    /**
-     * @image is divided into four parts
-     *  |---------------------|---|
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |---------------------|---|
-     *  |---------------------|---|
-     *  top left part: c can divide rows and cols in this part
-     *  top right part: c cannot divide rows in this part
-     *  bottom left part: c cannot divide cols in this part
-     *  bottom right part: c cannot divide rows and cols in this part
-     *
-     *  it is inefficient to use if clause and calculate inside the loop, also
-     *  traverse by row is needed for the bottom part but not all images
-     *  requir this kind of calculation which is slow. therefore, the code
-     *  in this section is long. However, it is more efficient because the image
-     *  can only meet one condition during the process
-     */
-    if (!mod_cols && !mod_rows) {
-        for (i = 0; i < m_rows; ++i) {
-            for (j = 0; j < m_cols; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
+            for (i = 0; i < x_rows; ++i) {
+                for (j = 0; j < y_cols; ++j) {
+                    int offset = (x * c + i) * cols + (y * c + j);
+                    block_sum_r += pixels_i[offset].r;
+                    block_sum_g += pixels_i[offset].g;
+                    block_sum_b += pixels_i[offset].b;
+                }
             }
-        }
-    } else if (mod_cols && !mod_rows) {
-        for (i = 0; i < m_rows; ++i) {
-            for (j = 0; j < m_cols - 1; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
+            for (i = 0; i < x_rows; ++i) {
+                for (j = 0; j < y_cols; ++j) {
+                    int offset = (x * c + i) * cols + (y * c + j);
+                    pixels_o[offset].r = (pixval)(block_sum_r / m_area);
+                    pixels_o[offset].g = (pixval)(block_sum_g / m_area);
+                    pixels_o[offset].b = (pixval)(block_sum_b / m_area);
+                }
             }
-        }
 
-        j = m_cols - 1;
-        for (i = 0; i < m_rows; ++i) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= c * mod_cols;
-            lpixels[offset].g /= c * mod_cols;
-            lpixels[offset].b /= c * mod_cols;
-        }
-    } else if (!mod_cols && mod_rows) {
-        for (i = 0; i < m_rows - 1; ++i) {
-            for (j = 0; j < m_cols; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
-            }
-        }
-
-        i = m_rows - 1;
-        for (j = 0; j < m_cols; ++j) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= mod_rows * c;
-            lpixels[offset].g /= mod_rows * c;
-            lpixels[offset].b /= mod_rows * c;
-        }
-    } else {
-        for (i = 0; i < m_rows - 1; ++i) {
-            for (j = 0; j < m_cols - 1; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
-            }
-        }
-
-        i = m_rows - 1;
-        for (j = 0; j < m_cols - 1; ++j) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= mod_rows * c;
-            lpixels[offset].g /= mod_rows * c;
-            lpixels[offset].b /= mod_rows * c;
-        }
-
-        j = m_cols - 1;
-        for (i = 0; i < m_rows - 1; ++i) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= c * mod_cols;
-            lpixels[offset].g /= c * mod_cols;
-            lpixels[offset].b /= c * mod_cols;
-        }
-        int offset = (m_cols - 1) * m_rows + m_rows - 1;
-        average_r += lpixels[offset].r;
-        average_g += lpixels[offset].g;
-        average_b += lpixels[offset].b;
-        lpixels[offset].r /= mod_rows * mod_cols;
-        lpixels[offset].g /= mod_rows * mod_cols;
-        lpixels[offset].b /= mod_rows * mod_cols;
-    }
-
-    // similar to the calculation part, assignment the rgb value back
-    for (i = 0; i < rows; ++i) {
-        for (j = 0; j < cols; ++j) {
-            int offset = i * cols + j;
-            int avg_offset = i / c * m_cols + j / c;
-            pixels_o[offset].r = (pixval)lpixels[avg_offset].r;
-            pixels_o[offset].g = (pixval)lpixels[avg_offset].g;
-            pixels_o[offset].b = (pixval)lpixels[avg_offset].b;
+            average_r += block_sum_r;
+            average_g += block_sum_g;
+            average_b += block_sum_b;
         }
     }
 
@@ -228,12 +79,10 @@ void mosaic_transform(pixel *pixels_o, pixel *pixels_i, int cols, int rows, int 
         (pixval)(average_g / area),
         (pixval)(average_b / area)
     };
-    duration = omp_get_wtime() - begin;
+    end = clock();
+    float seconds = (end - begin) / (float)CLOCKS_PER_SEC;
 
-    // clean up
-    free_lpixel(lpixels);
-
-    printf("CPU mode execution time took %.3lf ms\n", 1000 * duration);
+    printf("CPU mode execution time took %.3lf ms\n", 1000 * seconds);
     printf("CPU Average image colour red = %d, green = %d, blue = %d\n", average_color.r, average_color.g, average_color.b);
 }
 
@@ -251,175 +100,47 @@ void mosaic_transform(pixel *pixels_o, pixel *pixels_i, int cols, int rows, int 
  * @param c
  */
 void mosaic_transform_omp(pixel *pixels_o, pixel *pixels_i, int cols, int rows, int c) {
-    int i, j;                                           // loop counter
-    int mod_cols = cols % c;                            // remain part of width
-    int mod_rows = rows % c;                            // remain part of height
-    int m_cols = (mod_cols) ? cols / c + 1 : cols / c;  // check whether cols (rows) can be divided by c
-    int m_rows = (mod_rows) ? rows / c + 1 : rows / c;  // if not, plus one to ensure enough memory has benn allocated
-    lpixval m_area = c * c;
-    lpixval area = cols * rows;
-    lpixval average_r, average_g, average_b;
-    lpixel *lpixels;  // pixel for calculation
+    int x, y, i, j;                                     // loop counter
+    int m_rows = (rows % c) ? rows / c + 1 : rows / c;  // check whether cols (rows) can be divided by c
+    int m_cols = (cols % c) ? cols / c + 1 : cols / c;  // if not, plus one to ensure enough memory has benn allocated
+    int area = cols * rows;
+    unsigned long long average_r, average_g, average_b;
+    unsigned long long block_sum_r, block_sum_g, block_sum_b;
     double begin, duration;
 
     average_r = 0; average_g = 0; average_b = 0;
 
-    // memory allocation and value initialisation
-    lpixels = init_lpixel(m_cols, m_rows);
-
     begin = omp_get_wtime();
     // sum up all rgb values and stor in the average pixel
-#pragma omp parallel for private(i, j) schedule(static, c)
-    for (i = 0; i < rows; ++i) {
-        for (j = 0; j < cols; ++j) {
-            int offset = i * cols + j;
-            int avg_offset = i / c * m_cols + j / c;
-            lpixels[avg_offset].r += pixels_i[offset].r;
-            lpixels[avg_offset].g += pixels_i[offset].g;
-            lpixels[avg_offset].b += pixels_i[offset].b;
-        }
-    }
+#pragma omp parallel for private(x, y, i, j, block_sum_r, block_sum_g, block_sum_b) reduction(+ : average_r, average_g, average_b)
+    for (x = 0; x < m_rows; ++x) {
+        for (y = 0; y < m_cols; ++y) {
+            // int avg_offset = x * m_cols + y;
+            block_sum_r = 0, block_sum_g = 0, block_sum_b = 0;
+            int x_rows = (x == m_rows - 1) ? rows - x * c : c;
+            int y_cols = (y == m_cols - 1) ? cols - y * c : c;
+            int m_area = x_rows * y_cols;
 
-    /**
-     * @image is divided into four parts
-     *  |---------------------|---|
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |                     |   |
-     *  |---------------------|---|
-     *  |---------------------|---|
-     *  top left part: c can divide rows and cols in this part
-     *  top right part: c cannot divide rows in this part
-     *  bottom left part: c cannot divide cols in this part
-     *  bottom right part: c cannot divide rows and cols in this part
-     *
-     *  it is inefficient to use if clause and calculate inside the loop, also
-     *  traverse by row is needed for the bottom part but not all images
-     *  requir this kind of calculation which is slow. therefore, the code
-     *  in this section is long. However, it is more efficient because the image
-     *  can only meet one condition during the process
-     */
-    if (!mod_cols && !mod_rows) {
-#pragma omp parallel for private(i, j) reduction(+ : average_r, average_g, average_b)
-        for (i = 0; i < m_rows; ++i) {
-            for (j = 0; j < m_cols; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
+            for (i = 0; i < x_rows; ++i) {
+                for (j = 0; j < y_cols; ++j) {
+                    int offset = (x * c + i) * cols + (y * c + j);
+                    block_sum_r += pixels_i[offset].r;
+                    block_sum_g += pixels_i[offset].g;
+                    block_sum_b += pixels_i[offset].b;
+                }
             }
-        }
-    } else if (mod_cols && !mod_rows) {
-#pragma omp parallel for private(i, j) reduction(+ : average_r, average_g, average_b)
-        for (i = 0; i < m_rows; ++i) {
-            for (j = 0; j < m_cols - 1; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
+            for (i = 0; i < x_rows; ++i) {
+                for (j = 0; j < y_cols; ++j) {
+                    int offset = (x * c + i) * cols + (y * c + j);
+                    pixels_o[offset].r = (pixval)(block_sum_r / m_area);
+                    pixels_o[offset].g = (pixval)(block_sum_g / m_area);
+                    pixels_o[offset].b = (pixval)(block_sum_b / m_area);
+                }
             }
-        }
 
-        j = m_cols - 1;
-#pragma omp parallel for private(i) reduction(+ : average_r, average_g, average_b)
-        for (i = 0; i < m_rows; ++i) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= c * mod_cols;
-            lpixels[offset].g /= c * mod_cols;
-            lpixels[offset].b /= c * mod_cols;
-        }
-    } else if (!mod_cols && mod_rows) {
-#pragma omp parallel for private(i, j) reduction(+ : average_r, average_g, average_b)
-        for (i = 0; i < m_rows - 1; ++i) {
-            for (j = 0; j < m_cols; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
-            }
-        }
-
-        i = m_rows - 1;
-#pragma omp parallel for private(j) reduction(+ : average_r, average_g, average_b)
-        for (j = 0; j < m_cols; ++j) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= mod_rows * c;
-            lpixels[offset].g /= mod_rows * c;
-            lpixels[offset].b /= mod_rows * c;
-        }
-    } else {
-#pragma omp parallel for private(i, j) reduction(+ : average_r, average_g, average_b)
-        for (i = 0; i < m_rows - 1; ++i) {
-            for (j = 0; j < m_cols - 1; ++j) {
-                int offset = i * m_cols + j;
-                average_r += lpixels[offset].r;
-                average_g += lpixels[offset].g;
-                average_b += lpixels[offset].b;
-                lpixels[offset].r /= m_area;
-                lpixels[offset].g /= m_area;
-                lpixels[offset].b /= m_area;
-            }
-        }
-
-        i = m_rows - 1;
-#pragma omp parallel for private(j) reduction(+ : average_r, average_g, average_b)
-        for (j = 0; j < m_cols - 1; ++j) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= mod_rows * c;
-            lpixels[offset].g /= mod_rows * c;
-            lpixels[offset].b /= mod_rows * c;
-        }
-
-        j = m_cols - 1;
-#pragma omp parallel for private(i) reduction(+ : average_r, average_g, average_b)
-        for (i = 0; i < m_rows - 1; ++i) {
-            int offset = i * m_cols + j;
-            average_r += lpixels[offset].r;
-            average_g += lpixels[offset].g;
-            average_b += lpixels[offset].b;
-            lpixels[offset].r /= c * mod_cols;
-            lpixels[offset].g /= c * mod_cols;
-            lpixels[offset].b /= c * mod_cols;
-        }
-        int offset = (m_cols - 1) * m_rows + m_rows - 1;
-        average_r += lpixels[offset].r;
-        average_g += lpixels[offset].g;
-        average_b += lpixels[offset].b;
-        lpixels[offset].r /= mod_rows * mod_cols;
-        lpixels[offset].g /= mod_rows * mod_cols;
-        lpixels[offset].b /= mod_rows * mod_cols;
-    }
-
-    // similar to the calculation part, assignment the rgb value back
-#pragma omp parallel for private(i, j)
-    for (i = 0; i < rows; ++i) {
-        for (j = 0; j < cols; ++j) {
-            int offset = i * cols + j;
-            int avg_offset = i / c * m_cols + j / c;
-            pixels_o[offset].r = (pixval)lpixels[avg_offset].r;
-            pixels_o[offset].g = (pixval)lpixels[avg_offset].g;
-            pixels_o[offset].b = (pixval)lpixels[avg_offset].b;
+            average_r += block_sum_r;
+            average_g += block_sum_g;
+            average_b += block_sum_b;
         }
     }
 
@@ -432,9 +153,6 @@ void mosaic_transform_omp(pixel *pixels_o, pixel *pixels_i, int cols, int rows, 
         (pixval)(average_b / area)
     };
     duration = omp_get_wtime() - begin;
-
-    // clean up
-    free_lpixel(lpixels);
 
     printf("CPU mode execution time took %.3lf ms\n", 1000 * duration);
     printf("CPU Average image colour red = %d, green = %d, blue = %d\n", average_color.r, average_color.g, average_color.b);
--- src/filter.h
+++ src/filter.h
@@ -18,22 +18,6 @@ extern "C" {
 
 #include "libppm.h"
 
-// data type for color and rgb structure
-// lpixval means long pixel value, which can store large integer
-// used to sum up pixels and calculate average
-// uint32 range from 0-4294967295, which is enough for image with 4096x4096
-// so it is waste of memory for using unsigned long long
-typedef unsigned long long lpixval;
-typedef struct _lpixel {
-    lpixval r, g, b;
-} lpixel;
-
-// memory allocation and free for lpixels
-// init_lpixel() not only allocate memory but also initialise the value to 0
-// by calloc() which is used to calculate the rgb value for average
-lpixel *init_lpixel(int cols, int rows);
-void free_lpixel(lpixel *pixels);
-
 // image average value calculation and mosaic filter and their OpenMP version
 void mosaic_transform(pixel *pixels_o, pixel *pixels_i, int cols, int rows, int c);
 void mosaic_transform_omp(pixel *pixels_o, pixel *pixels_i, int cols, int rows, int c);
